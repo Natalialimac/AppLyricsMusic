2d44c71d178a618ac458cbd4a1466940fc05faf1
FelipeKaian <62892814+FelipeKaian@users.noreply.github.com>
2021-11-20 13:55:17 -0300

    Implementação das chamadas da API

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -10,6 +10,33 @@ import {
 } from './styles';
 
 const Home = () => {
+  
+  const [artist,setArtist] = useState("");
+
+    const [music,setMusic] = useState("");
+
+    const [text,setText] = useState("")
+
+    useEffect(()=>{
+        axios.get("https://api.vagalume.com.br/search.php?apikey=660a4395f992ff67786584e238f501aa&art="+artist+"&mus="+music).then(res =>{
+            console.log(res.data)
+
+            if(res.data.type === "notfound" || res.data === undefined || res.data.mus === undefined){
+                setText("Nenhuma música encontrada com essas informações");
+            }else if( res.data.mus.length > 0){
+               setText(res.data.mus[0].text);
+            }   
+        })      
+    },[artist,music]);
+
+    function handleOnArtistChange(e) {
+        setArtist(e.target.value)
+    }
+
+    function handleOnMusicChange(e) {
+        setMusic(e.target.value)
+    }
+  
   return (
     <Container>
       <ContainerArtist>

580b2fa003c7fb84d969949c011450c73b7fef91
Natália Lima <n.lliimadc@gmail.com>
2021-11-23 23:10:05 -0300

    Ajuste do CSS e funções para buscar letar no endpoint

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,45 +1,34 @@
 import React from 'react';
-import styled from 'styled-components/native';
+import {StyleSheet} from 'react-native';
 
-export const Container = styled.SafeAreaView`
-  background-color: #63cbcb;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const ContainerArtist = styled.SafeAreaView`
-  padding: 15px;
-`;
-
-export const ContainerMusic = styled.SafeAreaView`
-  padding-top: 5px;
-`;
-
-export const CustomButton = styled.TouchableOpacity`
-  margin-top: 30px;
-  height: 60px;
-  width: 25%;
-  background-color: #268596;
-  border-radius: 30px;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const CustomButtonText = styled.Text`
-  color: #fff;
-  font-size: 16px;
-`;
-
-export const InputArea = styled.TextInput`
-  /* flex: 1; */
-  color: #fff;
-  font-size: 16px;
-  color: #268596;
-  margin-top: 15px;
-  /* margin-left: 10px; */
-  width: 100%;
-  padding: 10px;
-  border-radius: 20px;
-  background-color: #fff;
-`;
+export const styles = StyleSheet.create({
+  area: {
+    flex: 1,
+    backgroundColor: '#63cbcb',
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderRadius: 55,
+    height: 40,
+    margin: 20,
+    padding: 10,
+  },
+  scrollView: {
+    marginTop: 50,
+    borderRadius: 55,
+    marginHorizontal: 20,
+    height: 400,
+  },
+  text1: {
+    color: '#fff',
+    fontSize: 20,
+    marginTop: 25,
+    marginBottom: 25,
+    textAlign: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+});
